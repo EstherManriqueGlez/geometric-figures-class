5ae7b1155061678bdfb24b0a4512ac065f43e866
Esther Manrique <esther_manrique@epam.com>
2022-06-05 20:17:06 -0500

    First commit

---
+++ figures.js
@@ -0,0 +1,25 @@
+import { Circle } from './modules/circles.js';
+import { Rectangle } from './modules/rectangles.js';
+import { Triangle } from './modules/triangles.js';
+
+class Circle1 extends Circle {
+  constructor() {
+    super(); 
+
+  }
+}
+
+class Rectangle1 extends Rectangle {
+  constructor() {
+    super(); 
+
+  }
+}
+
+class Triangle1 extends Triangle {
+  constructor() {
+    super(); 
+
+  }
+}
+
---
+++ modules/circles.js
@@ -0,0 +1,11 @@
+class Circle {
+  constructor(xc, yc) {
+    this.xc = xc;
+    this.yc = yc;
+  }
+  drawCircle() {
+    
+  }
+}
+
+export { Circle }
